47ef94157bd80d18506f30e2d6dc9a1f0ce59552
Philipp Otto <philotto95@gmail.com>
2022-02-06 22:32:49 +0100

    Add caesar_cipher_single_letter method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -11,3 +11,12 @@ def caesar_cipher_single_letter_upcase(letter, shift)
   distance_to_zero = "A".ord
   return ((((letter.ord-distance_to_zero)+shift)%26)+distance_to_zero).chr
 end
+
+def caesar_cipher_single_letter(letter,shift)
+  if capital?(letter)
+    return caesar_cipher_single_letter_upcase(letter,shift)
+  else
+    return caesar_cipher_single_letter_downcase(letter,shift)
+  end
+end
+
